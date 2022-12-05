173fccb28b60aee481015ff9964ddd4dd84aab3f
Miguel <miarojasb@correo.udistrital.edu.co>
2022-12-04 22:22:34 -0500

    API

--- public/javascripts/map.js
+++ public/javascripts/map.js
@@ -1,9 +1,18 @@
-var map = L.map('main_map').setView([4.579524, -74.157496], 13);
+var map = L.map('main_map').setView([4.579524, -74.157496], 15);
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',{
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-L.marker([4.579524,-74.157496]).addTo(map);
-L.marker([4.582671,-74.158065]).addTo(map);
-L.marker([4.584111,-74.159323]).addTo(map);
+
+
+$.ajax({
+    dataType: "json",
+    url: "api/bicicletas",
+    success: function(result){
+        console.log(result);
+        result.bicicletas.forEach(function(bici){
+            L.marker(bici.ubicacion, {title: bici.id}).addTo(map);
+        });
+    }
+})
